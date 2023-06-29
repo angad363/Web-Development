5385689f27211c33e38bf91394f19e0d8e1430c6
Angad Gosain <angadgosain@gmail.com>
2023-06-28 21:11:48 -0400

    Travel Advisor App

--- React JS Projects/travel_advisor/src/App.js
+++ React JS Projects/travel_advisor/src/App.js
@@ -1,25 +1,84 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import { CssBaseline, Grid } from "@material-ui/core";
+import {getPlacesData} from "./api";
 
 import Header from './components/Header/Header';
 import List from './components/List/List';
 import Map from './components/Map/Map';
 
 const App = () => {
+    const [places, setPlaces] = useState([]);
+    const [filteredPlaces, setFilteredPlaces] = useState([]);
+    const [childClicked, setChildClicked] = useState(null);
+
+    const [coordinates, setCoordinates] = useState({});
+    const [bounds, setBounds] = useState({});
+
+    const [type, setType] = useState('restaurants');
+    const [rating, setRating] = useState('');
+
+    const [isLoading, setIsLoading] = useState(false);
+
+    useEffect(() => {
+        navigator.geolocation.getCurrentPosition(({coords: {latitude, longitude}}) => {
+            setCoordinates({lat: latitude, lng: longitude});
+        })
+    }, [])
+
+    useEffect(() => {  
+        if(bounds.sw && bounds.ne){
+            //console.log(coordinates, bounds);
+            setIsLoading(true);
+            getPlacesData(type, bounds.sw, bounds.ne)
+            .then((data) => {
+                //console.log(data);
+                setPlaces(data && data.filter((place) => place.name && place.num_reviews > 0));
+                setFilteredPlaces([]);
+                setIsLoading(false);
+            })
+        }
+        
+    }, [type, bounds]);
+
+    console.log(places);
+    console.log(filteredPlaces);
+
+    useEffect(() => {
+        const filteredPlaces = places.filter((place) => place.rating > rating);
+        
+        setFilteredPlaces(filteredPlaces);
+    
+    }, [rating]);
+
     return (
         <React.Fragment>
         <CssBaseline />
-            <Header />
+            <Header 
+                setCoordinates = {setCoordinates}/>
             <Grid container spacing={3} style={{ width: '100%'}}>
                 <Grid item xs={12} md={4}>
-                    <List />
+                    <List 
+                        places={places}
+                        childClicked={childClicked}
+                        isLoading = {isLoading}
+                        type={type}
+                        setType = {setType}
+                        rating = {rating}
+                        setRating = {setRating}
+                    />
 
                 </Grid>
 
                 <Grid item xs={12} md={8}>
-                    <Map />
-                    
+                    <Map
+                        setCoordinates = {setCoordinates}
+                        setBounds = {setBounds}
+                        coordinates = {coordinates}
+                        places={filteredPlaces.length? filteredPlaces : places}
+                        setChildClicked = {setChildClicked}
+                    />
                 </Grid>
+
             </Grid>
         </React.Fragment>
     )
--- React JS Projects/travel_advisor/src/components/Map/Map.jsx
+++ React JS Projects/travel_advisor/src/components/Map/Map.jsx
@@ -1,34 +1,65 @@
-import React from "react";
+import React, { useState } from "react";
 import GoogleMapReact from "google-map-react";
 import { Paper, Typography, useMediaQuery } from "@material-ui/core";
 import LocationOnOutlinedIcon from "@material-ui/icons/LocationOnOutlined";
-import { Rating } from "@material-ui/lab";
+import Rating from '@material-ui/lab/Rating';
 
 import useStyles from "./styles";
-import { GoogleMap } from "@react-google-maps/api";
-const Map = () => {
-    const classes = useStyles();
-    const isMobile = useMediaQuery('(min-width: 600px)');
-    
-    const coordinates = {lat: 0, lng: 0};
-    return (
-        <div className={classes.mapContainer}>
-            <GoogleMapReact
-            bootstrapURLKeys = {{key: 'AIzaSyDSdLzDPvDf1pnyVI_p0-6mhv-mj3_yUCI'}}
-            dedfaultCenter={coordinates}
-            center={coordinates}
-            defaultZoom={14}
-            margin={[50, 50, 50, 50]}
-            options={''}
-            onChange={''}
-            
-            onChildClick={''}
-            >
-            
 
-            </GoogleMapReact>
-        </div>
-    );
-}
+const Map = ({coordinates, setCoordinates, setBounds, places, setChildClicked}) => {
+  console.log(coordinates)
+  console.log(setCoordinates);
+  const classes = useStyles();
+  const isDesktop = useMediaQuery("(min-width: 600px)");
+  
+
+  //const coordinates = {lat: 0, lng: 0}
+  return (
+    <div className={classes.mapContainer}>
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: process.env.REACT_APP_GOOGLE_MAPS_API_KEY }}
+        defaultCenter={coordinates}
+        center={coordinates}
+        defaultZoom={14}
+        margin={[50, 50, 50, 50]}
+        options={{}}
+        onChange={(e) => {
+          setCoordinates({ lat: e.center.lat, lng: e.center.lng});
+          setBounds({ne: e.marginBounds.ne, sw: e.marginBounds.sw});
+        }}
+        onChildClick={(child) => setChildClicked(child)}
+      >
+        {places && places.map((place, i) => (
+          <div
+          className={classes.markerContainer}
+          lat={Number(place.latitude)}
+          lng={Number(place.longitude)}
+          key={i}
+          >
+            {
+              !isDesktop ? (
+                <LocationOnOutlinedIcon color="primary" fontSize="large"/>
+              ):
+              (
+                <Paper elevation={3} className={classes.paper}>
+                  <Typography className={classes.typography} variant="subtitle2" gutterBottom>
+                    {place.name}
+                  </Typography>
+                  <img 
+                    className={classes.pointer}
+                    src={place.photo ? place.photo.images.large.url : 'https://www.foodserviceandhospitality.com/wp-content/uploads/2016/09/Restaurant-Placeholder-001.jpg'}
+                    alt={place.name}
+
+                  />
+                  <Rating size="small" value= {Number(place.rating)} readOnly/>
+                </Paper>
+              )
+            }
+          </div>
+        ))}
+      </GoogleMapReact>
+    </div>
+  );
+};
 
 export default Map;
